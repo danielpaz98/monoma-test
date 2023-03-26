dd35e16c96dbb722c8e784618c60cbe8675a5959
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-03-26 01:30:57 -0500

    Add PokedexView styles

---
+++ src/components/PokedexView/index.ts
@@ -0,0 +1,7 @@
+import tw, { styled } from "twin.macro";
+
+export const PokedexView = styled.div`
+	${tw`flex justify-center flex-wrap gap-6 rounded bg-neutral-2 p-4`}
+`;
+
+export default PokedexView;
