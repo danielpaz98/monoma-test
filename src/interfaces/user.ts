4fca1216caa65638762ad00ac1e1be5c25228ace
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-03-26 01:33:15 -0500

    Add global interfaces

---
+++ src/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+	id: string | undefined;
+	firstName: string | undefined;
+	lastName: string | undefined;
+	avatar: string | undefined;
+	email: string | undefined;
+	password: string | undefined;
+	createdAt: string | undefined;
+	accessToken: string | undefined;
+}
