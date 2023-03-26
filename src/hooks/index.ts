3a1d91a79bc21d43c90b8bfcede765d422c77547
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-03-26 01:32:30 -0500

    Add barrel hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+export { default as useAuth } from "./useAuth";
+export { default as useOnClickOutside } from "./useOnClickOutside";
+export { default as usePokemon } from "./usePokemon";
+export { default as usePokemons } from "./usePokemons";
