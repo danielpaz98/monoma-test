ea90fe6cf4a4b346ff157d67700f0078200ef377
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-03-26 01:35:42 -0500

    Add barrel for pages

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as Error404Page } from "./Error404Page";
+export { default as HomePage } from "./HomePage";
+export { default as PokemonPage } from "./PokemonPage";
+export { default as ProfilePage } from "./ProfilePage";
+export { default as SignInPage } from "./SignInPage";
