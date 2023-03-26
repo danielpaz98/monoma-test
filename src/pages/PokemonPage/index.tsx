563c7f896c156b4d4c6c0ce00e7db7bf5745ae0a
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-03-26 01:35:52 -0500

    Add Pokemon page

---
+++ src/pages/PokemonPage/index.tsx
@@ -0,0 +1,46 @@
+// PLUGINS
+import { Navigate } from "react-router-dom";
+// LAYOUTS
+import MainLayout from "~/layouts/MainLayout";
+// COMPONENTS
+import { DetailCard, Loader } from "~/components";
+// HOOKS
+import usePokemon from "~/hooks/usePokemon";
+
+const PokemonPage = () => {
+	const { pokemon, isLoading, isError } = usePokemon();
+
+	if (isError) return <Navigate replace to="/404" />;
+
+	if (isLoading)
+		return (
+			<Loader
+				className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
+				isLoading={isLoading}
+				text="Cargando..."
+			/>
+		);
+
+	if (!pokemon) return null;
+
+	return (
+		<MainLayout>
+			<DetailCard
+				abilities={pokemon.abilities}
+				className="mx-auto"
+				id={pokemon.id}
+				image={
+					pokemon.sprites.other?.home?.front_default ||
+					pokemon.sprites?.other?.dream_world?.front_default ||
+					pokemon.sprites.other?.["official-artwork"]?.front_default
+				}
+				moves={pokemon.moves}
+				name={pokemon.name}
+				types={pokemon.types}
+				weight={pokemon.weight}
+			/>
+		</MainLayout>
+	);
+};
+
+export default PokemonPage;
